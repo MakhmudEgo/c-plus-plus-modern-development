677eeabb7b1b224a930f9d3420b97c89c6189f33
Makhmud Ego <makhmudego@MacBook-Pro-Makhmud.local>
2020-04-20 01:52:07 +0300

    exercise factorial

--- c-plus-plus-white/2_week/factorial.cpp
+++ c-plus-plus-white/2_week/factorial.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int Factorial(int n)
+{
+    int factorial = 1;
+
+    if (n < 2)
+        return (1);
+    else
+    {
+         while (n >=1)
+         {
+             factorial *= n;
+             --n;
+         }
+    }
+    return (factorial);
+}
+
 int     main(void)
 {
-    cout << "hello gitignore!" << endl;
+    cout << Factorial(4) << endl;
+    cout << Factorial(-2) << endl;
+    cout << Factorial(1) << endl;
+    cout << Factorial(0) << endl;
+    cout << Factorial(10) << endl;
     return (0);
 }
