9adb2c32695d4a4daada8f20ac005d0e4477c838
makhmudego <makhmudego@yandex.ru>
2020-04-30 19:50:07 +0300

    new exercise "initialization methods, and end 3 week

---
+++ c-plus-plus-white/3_week/initialization_methods.cpp
@@ -0,0 +1,12 @@
+struct Incognizable {
+    int i = 1;
+    int n = 2;
+};
+
+int main() {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+    return 0;
+}
